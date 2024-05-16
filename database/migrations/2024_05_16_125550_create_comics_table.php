f858cc1e9d90b90e6cad63c4bb9ae9aa2bb802ec
s4r44d43 <sararez@live.it>
2024-05-16 15:16:58 +0200

    [added] table

--- database/migrations/2024_05_16_125550_create_comics_table.php
+++ database/migrations/2024_05_16_125550_create_comics_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->decimal('price', 4, 2);
+            $table->string('series', 80);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->string('artists');
+            $table->string('writers');
+
+            
             $table->timestamps();
         });
     }
